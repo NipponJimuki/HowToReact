1f39e8a6af46e10484f4daf57710ad9ef94d5a0d
NJC4746 <kaida.ryo.ichi@njc.co.jp>
2018-11-21 10:10:33 +0900

    faat(app): :tada: HowToReact5(CSS-in-JS)整備

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 // App.js
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
-import styles from './style.css';
+import styled from 'styled-components';
 
 class App extends PureComponent {
     render() {
         return (
-            <div className={styles.helloDiv}>
+            <HelloDiv>
                 Hello
-                <span className={styles.helloSpan}>React</span>
+                <HelloSpan>React</HelloSpan>
                 World!
-            </div>
+            </HelloDiv>
         );
     }
 }
+
+const HelloDiv = styled.div`
+    background-color: #008080;
+    color: white;
+    font-family: Roboto;
+    height: 50px;
+    margin: 5rem;
+    line-height: 50px;
+    text-align: center;
+    transition: all 450ms cubic-bezier(0.23, 1, 0.32, 1, 100ms);
+    :hover {
+        background-color: #00a0a0;
+    }
+`;
+
+const HelloSpan = styled.span`
+    font-size: 30px;
+    font-style: italic;
+    margin: 1rem;
+`;
 
 ReactDOM.render(<App />, document.getElementById('content'));
