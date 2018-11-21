ee8ab4871833131fe39b3938df200bdf43d67157
NJC4746 <kaida.ryo.ichi@njc.co.jp>
2018-11-21 09:59:28 +0900

    faat(app): :tada: HowToReact1整備

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// App.js
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return <div>Hello React World!</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('content'));
