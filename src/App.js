7131db0018464e575bd684e13110ccc338aa6ad4
NJC4746 <kaida.ryo.ichi@njc.co.jp>
2018-11-21 10:04:05 +0900

    faat(app): :tada: HowToReact5(CSS-Modules)整備

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 // App.js
 import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
-import HRC from './HRC';
+import styles from './style.css';
 
 class App extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: 'off',
-        };
-
-        this._handleChange = this._handleChange.bind(this);
-    }
-
-    _handleChange(value) {
-        this.setState({ text: value });
-    }
-
     render() {
         return (
-            <div>
-                <HRC handleChange={this._handleChange} />
-                <div>
-                    App/
-                    {this.state.text}
-                </div>
+            <div className={styles.helloDiv}>
+                Hello
+                <span className={styles.helloSpan}>React</span>
+                World!
             </div>
         );
     }
